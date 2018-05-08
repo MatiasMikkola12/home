0b78a5d6a7b31cd403154a0400441d1c5d5d51b3
Matias  Mikkola <matu.mikkola@gmail.com>
2018-05-07 19:53:44 -0700

    Add downarrow

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './Home.css';
 import { fadeInUp, fadeInDown } from 'react-animations';
 import Radium, {StyleRoot} from 'radium';
+import TiArrowSortedDown from 'react-icons/lib/ti/arrow-sorted-down';
 
 const styles = {
   fadeInUp: {
-    animation: 'x 1.5s',
+    animation: 'x 2s',
     animationName: Radium.keyframes(fadeInUp, 'fadeInUp')
   },
   fadeInDown: {
-    animation: 'x 1.5s',
+    animation: 'x 2s',
     animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+  },
+
+  fadeInDownQuick: {
+    animation: 'x 4s',
+    animationName: Radium.keyframes(fadeInDown, 'fadeInDownQuick')
   }
 }
 
@@ -20,16 +26,21 @@ class Home extends Component {
           <div className="hero">
             <div className="header">
               <StyleRoot>
-                <div className="test" style={styles.fadeInUp}>
+                <div className="test" style={styles.fadeInDown}>
                   <h1>Matias Mikkola</h1>
                 </div>
               </StyleRoot>
               <StyleRoot>
-                <div className="test" style={styles.fadeInDown}>
+                <div className="test" style={styles.fadeInUp}>
                   <h1>UI Developer</h1>
                 </div>
               </StyleRoot>
             </div>
+            <StyleRoot>
+              <div className="down" style={styles.fadeInDownQuick}>
+                <TiArrowSortedDown />
+              </div>
+            </StyleRoot>
           </div>
         )
     }
